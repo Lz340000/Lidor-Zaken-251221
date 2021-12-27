46ff20ea5a9ba598c095773cc03526a836ee676e
Lidor Zaken <lidorzaken@Lidors-MacBook-Pro.local>
2021-12-27 15:24:13 +0200

    heroku

--- src/errorHandler/errorList.js
+++ src/errorHandler/errorList.js
@@ -2,7 +2,8 @@ export const REDIRECT = 'redirect';
   export const RETURN_MESSAGE = 'message';
   export const THROW = 'throw';
   export const httpStatus = {
-    403: { action: REDIRECT, path: '/login' },
+    403: { action: REDIRECT, path: '/error-page' },
+    404: { action: REDIRECT, path: '/error-page' },
     500: { action: REDIRECT, path: '/error-page' }
   };
   export const errorList = {
